ee9bc8d7c65670b516ab9c8bbfea3ec920ce9619
Rajat k <rj_rajat18@yahoo.in>
2016-12-15 18:50:37 +0530

    Removes Critical Details
    New user can add the server details.

--- Web Portal/includes/constants.php
+++ Web Portal/includes/constants.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_SERVER", "182.50.133.91");
-define("DB_USER", "secate");
-define("DB_PASSWORD", "rahul@123");
-define("DB_NAME", "SE_project");
+define("DB_SERVER", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+define("DB_NAME", "");
 
 ?>
